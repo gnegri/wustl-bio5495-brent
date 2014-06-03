ea44306d5fa3e92387e36e5fdd36a814dd128807
gnegri <grantnegri@gmail.com>
2014-06-03 15:58:55 -0500

    i dont even know man, it works in a .nb

--- mod01Probability/dicePosteriorAssignment/dicePosterior.m
+++ mod01Probability/dicePosteriorAssignment/dicePosterior.m
@@ -1,10 +1,9 @@
-
 (*A draw is the series of rolls between the time a die is drawn from the bag and the time it is returned to the bag.
   dicePosterior calculates the posterior probability of a type1 versus type2 die, based the number of times each face
   appears in that draw and the relative numbers of Type 1 and Type 2 dice in the bag as well as the face probabilities
   for Type 1 and Type 2 dice. The single number returned is the posterior probability of Type 1.*)
 dicePosterior[binCounts_, type1Prior_, type2Prior_, faceProbs1_, faceProbs2_] := 
-	Module[{expHelper, sides, pBgT1, pBgT2, pT1gB}, 
+	Module[{sides, pBgT1, pBgT2, pT1gB}, 
  	expHelper[a_, b_] := If[a==0 && b==0, 1, a^b];
  	sides = Length[binCounts];
  	
