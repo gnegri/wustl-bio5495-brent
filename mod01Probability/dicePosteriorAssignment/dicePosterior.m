c83880a3d06528d235924e1f6de1b8cb3f694e70
gnegri <grantnegri@gmail.com>
2014-06-04 11:26:51 -0500

    giving up. works in .nb kernel

--- mod01Probability/dicePosteriorAssignment/dicePosterior.m
+++ mod01Probability/dicePosteriorAssignment/dicePosterior.m
@@ -3,12 +3,13 @@
   appears in that draw and the relative numbers of Type 1 and Type 2 dice in the bag as well as the face probabilities
   for Type 1 and Type 2 dice. The single number returned is the posterior probability of Type 1.*)
 dicePosterior[binCounts_, type1Prior_, type2Prior_, faceProbs1_, faceProbs2_] := 
-	Module[{sides, pBgT1, pBgT2, pT1gB}, 
+	Module[{sides, pBgT1, pBgT2}, 
  	expHelper[a_, b_] := If[a==0 && b==0, 1, a^b];
  	sides = Length[binCounts];
  	
  	pBgT1 = Product[expHelper[faceProbs1[[j]],binCounts[[j]]], {j, sides}];
 	pBgT2 = Product[expHelper[faceProbs2[[k]],binCounts[[k]]], {k, sides}];
 	
-	pT1gB = (pBgT1*type1Prior)/(pBgT1*type1Prior + pBgT2*type2Prior)
+	(pBgT1*type1Prior)/(pBgT1*type1Prior + pBgT2*type2Prior)
 ]
+(* works in a .nb file but gives indeterminate in tests *)
