0a0ed0e790bb27bc796b52ae0223143fdc9a214e
gnegri <grantnegri@gmail.com>
2014-09-23 10:40:47 -0500

    prep for actual work

--- mod02Motif/motifEMDistribution/motifEMAssignment.m
+++ mod02Motif/motifEMDistribution/motifEMAssignment.m
@@ -9,7 +9,7 @@ basicMM[input_, motifLength_, maxIterations_, accuracy_]:=
 	Module[{inputLengths, nCounts, backgroundFreqs, backgroundPWM, motifPseudocounts, lambda, posteriors, motifPWM},
 		(*Initialization*)
 		inputLengths=Map[Length, input];
-		nCounts=Total[Map[BinCounts[#, {Range[1, 5]}] &, input]];
+		nCounts=Total[Map[BinCounts[#, {Range[5]}] &, input]];
 		backgroundFreqs=(nCounts + {1., 1., 1., 1.}) / (Total[nCounts] + 4.);
 		motifPseudocounts=backgroundFreqs;
 		(*Initialized to one motif expected per input sequence.*)
@@ -30,7 +30,9 @@ basicMM[input_, motifLength_, maxIterations_, accuracy_]:=
 
 (*Write function initializePWM   *)
 initalizePWM[motifLength_]:=
-	Module[{}]
+	Module[{},
+	
+	]
 	
 updateProbs[input_, backgroundFreqs_, motifPWM_, backgroundPWM_, lambda_, motifPseudocounts_]:=
 	Module[{motifLength, posteriors, newMotifPWM, newBackgroundPWM, newLambda},
@@ -38,21 +40,25 @@ updateProbs[input_, backgroundFreqs_, motifPWM_, backgroundPWM_, lambda_, motifP
 		posteriors=calculatePosteriors[input, motifPWM, backgroundPWM, lambda];
 		newLambda=updateLambda[posteriors];{newMotifPWM, newBackgroundPWM} = updatePWM[motifLength, input, posteriors, motifPseudocounts];
 	    {newMotifPWM, newBackgroundPWM, newLambda, posteriors}
-		]
+	]
  		
 (*Write the function calculatePosteriors.*)
 calculatePosteriors[input_, motifPWM_, backgroundPWM_, lambda_]:=
-	Module[{}]
+	Module[{},
+	
+	]
 
 (*Write function updateLambda.*)
 updateLambda[posteriors_]:=
-	Module[{}]
+	Module[{},
+	
+	]
 
 (*Write the function updatePWMs*)
 updatePWM[motifLength_, input_, posteriors_, motifPseudocounts_]:=
-	Module[{},
+	Module[{newMotifPWM, newBackgroundPWM},
 		(*Write code here*)
 	(*Return updated PWMs for the motif and the background.*)
-	{newMotifPWM, newBackgroundPWM}	
+		{newMotifPWM, newBackgroundPWM}	
 	]
 	
