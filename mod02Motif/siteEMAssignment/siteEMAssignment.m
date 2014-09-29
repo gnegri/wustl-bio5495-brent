0b6b392810265c61d66b95f9018f49677de8b774
gnegri <grantnegri@gmail.com>
2014-09-29 10:06:20 -0500

    added sitePosterior/Sample

--- mod02Motif/siteEMAssignment/siteEMAssignment.m
+++ mod02Motif/siteEMAssignment/siteEMAssignment.m
@@ -38,3 +38,35 @@ updateProbs[sequences_, oldSiteProb_, oldBackgroundProb_, oldSiteFreqs_, oldBack
   		
   ]
 (* Make sure to include your siteSample and sitePosterior functions here.*)
+siteSample[siteProb_, backgroundProb_, siteFreqs_, backgroundFreqs_, numDraws_] := 
+	Module[{nucelotides=Range[4], seqDist, picks, dist, siteDist, backgroundDist, numBases = Length[siteFreqs]},
+		(*Verifying data input is in the correct form*)
+		If[Length[backgroundFreqs]!=4 || Total[backgroundFreqs]!=1, Return["Improper backgroundFreqs"]];
+		If[Length[siteFreqs\[Transpose]]!=4 || Total[Total[siteFreqs\[Transpose]]]!=Length[siteFreqs], Return["Improper siteFreqs sublist[s]"]];
+		
+		(* map each probability to 1 or 2 *)
+		seqDist = EmpiricalDistribution[{siteProb, backgroundProb}->{1,2}];
+		(* then pick numDraws_ number of either site or background seqs based on their probabilty *)
+		picks = Thread[RandomVariate[seqDist, numDraws]];
+		
+		(* map each nucelotide probability to the nucleotides (represented as 1,2,3,4) for each site *)
+		siteDist = EmpiricalDistribution[#->nucelotides]&/@siteFreqs;	
+		(* creates a list of {{siteDist_n, backgroundDist},...} *)
+		backgroundDist = Table[EmpiricalDistribution[backgroundFreqs->nucelotides],{numBases}];
+		dist = {siteDist,backgroundDist};
+		
+		(* for each pick, generate a sequence based on the specific nuceleotide frequency *)
+		Table[RandomVariate[dist[[#,i]]],{i,numBases}]&/@picks
+	]
+
+sitePosterior[sequence_, sitePrior_, backgroundPrior_, siteProbs_, backgroundProbs_] := 
+ 	Module[{len = Length[sequence],pSgBG,pSgS},
+ 		
+ 		(* sequence[[i]] is the base at that position; use that to get the 
+ 			probability of drawing that base from each distribution. *)
+ 		pSgBG = Product[backgroundProbs[[sequence[[i]]]],{i,len}];
+ 		pSgS  = Product[siteProbs[[i,sequence[[i]]]],{i,len}];
+ 		
+ 		(* zero event for both = .5; return the posterior probability otherwise *)
+ 		If[pSgBG == 0 && pSgS==0, .5, (sitePrior*pSgS)/(sitePrior*pSgS + backgroundPrior*pSgBG)]
+ 	]
