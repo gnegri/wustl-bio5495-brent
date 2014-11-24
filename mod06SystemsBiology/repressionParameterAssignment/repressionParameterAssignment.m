cdbf9d24bd5dbe4029bd867f0495a7c196204512
Grant Negri <grantnegri@gmail.com>
2014-11-24 01:47:10 -0600

    repressionParameterAssignment done. last unit test is questionable

--- mod06SystemsBiology/repressionParameterAssignment/repressionParameterAssignment.m
+++ mod06SystemsBiology/repressionParameterAssignment/repressionParameterAssignment.m
@@ -3,8 +3,17 @@
 (*NOTE: the functions list here are just the skeleton. You will probably want to 
  write other functions and call them to implement the functions listed here. *)
 
-generateRandomParams[n_, regulatorsMask_, seed_]:=
-	(* Write code here *)
+(* does what the function name implies. n genes, binary regulator list, random seed (optional) *)
+generateRandomParams[n_, regulatorsMask_, seed_:"Grant Negri wrote this"]:= 
+	Module[{bMatrix, cMatrix},
+		SeedRandom[seed, Method->"MersenneTwister"];
+		bMatrix = Table[RandomReal[0.01, 1],{n}];
+		cMatrix = Table[RandomReal[#]&/@regulatorsMask,{n}];
+		(* post-process *)
+		Do[cMatrix[[i,i]]=0, {i,n}];
+		
+		{bMatrix, cMatrix}
+	]
 
 
 (* mutantEqns takes a genePresenceMask indicating which genes were deleted in the cells 
@@ -13,21 +22,36 @@ generateRandomParams[n_, regulatorsMask_, seed_]:=
    that any actual measurments made on those deleted genes are ignored and any solver 
    doesn't have to deal with them.
 *)
-mutantEqns[genePresenceMask_]:=
-	(* Write code here *)
+mutantEqns[genePresenceMask_]:= 
+	Module[{activeGenes = Flatten[Position[genePresenceMask,1],1], n, interactions, activeParted, latterHalf},
+		n = Length[activeGenes];
+		(* hackish solution *)
+		(* single case handling *)
+		If[n==1, Return[{-1.0 + b[activeGenes[[1]]]m[activeGenes[[1]]]==0}]];
+		(* get a list of all possible 1TF-1Target interactions *)
+		interactions = Partition[Permutations[activeGenes, {2}], n-1];
+		activeParted = Complement[activeGenes, #] &/@ Partition[activeGenes,1];
+		(* fill out the product portion *)
+		latterHalf = Times @@@ Flatten[1 + {c @@@ # & /@ interactions}*{m /@ # & /@ activeParted}, 1];
+
+		Flatten[-1.0 + {b[#] m[#] & /@ activeGenes*latterHalf}, 1][[#]] == 0 & /@ Range[n]
+	]
 
 (* mutantExpVars returns a list of variables representing the expression levels of
    genes that remain in the genotype described by genePresenceMask. You will want 
    these later when you call FindRoot.*)
-mutantExpVars[genePresenceMask_]:=
-	(* Write code here *)
+mutantExpVars[genePresenceMask_]:= m[#]&/@Flatten[Position[genePresenceMask,1],1]
 
 (* expressionEqns replaces the symbolic parameters of the generic equations with actual values
    from a parameter matrix, returning a final set of equations with fixed parameters but variables
    for mRNA levels. Later, you will solve these for the mRNA variables yields to get a set of 
    expression levels that are consistent with the parameters.*)
 expressionEqns[eqns_, {bVector_, cMatrix_}]:=
-	(* Write code here *)
+	Module[{n=Length[eqns]},
+		Do[b[i] = bVector[[i]], {i, n}];
+		Do[c[i, j] = cMatrix[[i, j]], {i, n}, {j, n}];
+		eqns
+	]
 
 (* expressionMatrix takes parameters and a list of genePresenceMasks, one for each strain that we
    want an expression profile for. For each mask, it makes a set of equations and calls
@@ -37,5 +61,13 @@ expressionEqns[eqns_, {bVector_, cMatrix_}]:=
 expressionMatrix[params_, genePresenceMasks_]:=
 	(*Make sure b, c, m are undefined here and in all functions called from here.*)
 	Block[{b, c, m},
-	(* Write code here *)
-	]
+		Module[{mE, mEV, eqs, solveFor, replacementRules},
+		mE  = mutantEqns[#] &/@ genePresenceMasks;
+		eqs = expressionEqns[#, params] &/@ mE;
+		
+		mEV = mutantExpVars[#] &/@ genePresenceMasks;
+		solveFor = Transpose[{#, Table[0, {Length[#]}]}] &/@ mEV;
+		
+		replacementRules = MapThread[FindRoot, {eqs, solveFor}];
+		mEV[[#]] /. replacementRules[[#]] &/@ Range[Length[mEV]]
+	]]
